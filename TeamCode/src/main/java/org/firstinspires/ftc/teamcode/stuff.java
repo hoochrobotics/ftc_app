605c3b4303dd48cd7f785fe724b3c9435eb41548
DESKTOP-52JO1EN\ploop <hoochrobotics2018@gmail.com>
2018-10-12 22:11:22 -0400

    created a basic teleop for drive train

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stuff.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-public class stuff {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "TeleopStuff")
+public class TeleopStuff extends LinearOpMode
+{
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            leftMotor.setPower(-gamepad1.left_stick_y);
+            rightMotor.setPower(-gamepad1.right_stick_y);
+
+            idle();
+        }
+    }
 }
