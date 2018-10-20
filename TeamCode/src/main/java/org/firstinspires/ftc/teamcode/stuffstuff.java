a104f7f9a9bada37a389b32fece6aeb4131d8c03
DESKTOP-52JO1EN\ploop <hoochrobotics2018@gmail.com>
2018-10-19 21:32:57 -0400

    Added program for servo and died while trying to keep the brackets the way ian wanted them.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stuffstuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stuffstuff.java
@@ -11,6 +11,9 @@ public class stuffstuff extends LinearOpMode {
     private DcMotor leftMotor;
     private DcMotor rightMotor;
     private DcMotor liftMotor;
+    private Servo clawServo;
+    private static final double  ARM_EXTENDED_POSITION = 1.0;
+    private static final double ARM_RETRACTED_POSITION = 0.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,7 +21,9 @@ public class stuffstuff extends LinearOpMode {
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
         liftMotor = hardwareMap.dcMotor.get("liftMotor");
+        clawServo = hardwareMap.servo.get("clawServo");
 
+        clawServo.setPosition(ARM_RETRACTED_POSITION);
         //Reverses motors
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
 
@@ -28,7 +33,15 @@ public class stuffstuff extends LinearOpMode {
             //Maps motors to the game controller
             leftMotor.setPower(-gamepad1.left_stick_y);
             rightMotor.setPower(-gamepad1.right_stick_y);
-            liftMotor.setPower(0.4 * gamepad2.left_stick_y);
+            liftMotor.setPower (-(gamepad2.left_stick_y * -gamepad2.left_stick_y));
+
+            if (gamepad2.a) {
+                clawServo.setPosition(ARM_EXTENDED_POSITION);
+            }
+
+            if (gamepad2.b) {
+                clawServo.setPosition(ARM_RETRACTED_POSITION);
+            }
 
             idle();
         }
